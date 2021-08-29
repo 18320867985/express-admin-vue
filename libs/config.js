e89e1d7ebf1989d548fbc77f3c561b91af5d093a
hqs <3426158783@qq.com>
2021-08-29 16:17:42 +0800

    29

--- libs/config.js
+++ libs/config.js
@@ -1,10 +1,10 @@
 ﻿
 exports.mainConfig = {
     // url: "mongodb://localhost:27017,localhost:27018/admin?replicaSet=rs"   // 副本集
-    url: "mongodb://localhost:27017/admin",
+    url: "mongodb://localhost:27017/test",
 };
 
 exports.MongoClientConfig = {
     dbUrl: "mongodb://localhost:27017",
-    dbName: "admin"
+    dbName: "test"
 };
