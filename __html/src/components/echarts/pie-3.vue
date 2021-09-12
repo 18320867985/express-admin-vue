5ec8fc1db2b841548a6c93ba3b4338cd40172826
hqs <3426158783@qq.com>
2021-09-12 14:32:01 +0800

    9-12

---
+++ __html/src/components/echarts/pie-3.vue
@@ -0,0 +1,153 @@
+<template>
+<div :id="echartId" class="echart-box"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts/core';
+import {
+    DatasetComponent,
+    TooltipComponent,
+    GridComponent,
+    LegendComponent
+} from 'echarts/components';
+import {
+    LineChart,
+    PieChart
+} from 'echarts/charts';
+import {
+    CanvasRenderer
+} from 'echarts/renderers';
+
+echarts.use(
+    [DatasetComponent, TooltipComponent, GridComponent, LegendComponent, LineChart, PieChart, CanvasRenderer]
+);
+
+export default {
+
+    props: {
+        echartId: {
+            type: String,
+            default: "echartId"
+        }
+    },
+
+    mounted() {
+        var chartDom = document.getElementById(this.echartId);
+        var myChart = echarts.init(chartDom);
+        var option;
+
+        setTimeout(function () {
+
+            option = {
+                legend: {},
+                tooltip: {
+                    trigger: 'axis',
+                    showContent: false
+                },
+                dataset: {
+                    source: [
+                        ['产品', '2012', '2013', '2014', '2015', '2016', '2017'],
+                        ['成品奶茶', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+                        ['牛奶火柴', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+                        ['芝士可可', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
+                        ['比萨', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
+                    ]
+                },
+                xAxis: {
+                    type: 'category'
+                },
+                yAxis: {
+                    gridIndex: 0
+                },
+                grid: {
+                    top: '55%'
+                },
+                series: [{
+                        type: 'line',
+                        smooth: true,
+                        seriesLayoutBy: 'row',
+                        emphasis: {
+                            focus: 'series'
+                        }
+                    },
+                    {
+                        type: 'line',
+                        smooth: true,
+                        seriesLayoutBy: 'row',
+                        emphasis: {
+                            focus: 'series'
+                        }
+                    },
+                    {
+                        type: 'line',
+                        smooth: true,
+                        seriesLayoutBy: 'row',
+                        emphasis: {
+                            focus: 'series'
+                        }
+                    },
+                    {
+                        type: 'line',
+                        smooth: true,
+                        seriesLayoutBy: 'row',
+                        emphasis: {
+                            focus: 'series'
+                        }
+                    },
+                    {
+                        type: 'pie',
+                        id: 'pie',
+                        radius: '30%',
+                        center: ['50%', '25%'],
+                        emphasis: {
+                            focus: 'data'
+                        },
+                        label: {
+                            formatter: '{b}: {@2012} ({d}%)'
+                        },
+                        encode: {
+                            itemName: 'product',
+                            value: '2012',
+                            tooltip: '2012'
+                        }
+                    }
+                ]
+            };
+
+            myChart.on('updateAxisPointer', function (event) {
+                var xAxisInfo = event.axesInfo[0];
+                if (xAxisInfo) {
+                    var dimension = xAxisInfo.value + 1;
+                    myChart.setOption({
+                        series: {
+                            id: 'pie',
+                            label: {
+                                formatter: '{b}: {@[' + dimension + ']} ({d}%)'
+                            },
+                            encode: {
+                                value: dimension,
+                                tooltip: dimension
+                            }
+                        }
+                    });
+                }
+            });
+
+            myChart.setOption(option);
+
+        });
+
+        option && myChart.setOption(option);
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.echart-box {
+    height: 600px;
+    width: 100%;
+    padding-top:15px ;
+    background: #fff;
+
+}
+</style>>
--- __html/src/views/home.vue
+++ __html/src/views/home.vue
@@ -1,22 +1,30 @@
 <template>
 <div>
-    <h3 class="echart-ttl">线状图数据分析</h3>
-    <line-chart></line-chart>
 
     <h3 class="echart-ttl">柱状图数据分析</h3>
     <bar-chart echartId="bar"></bar-chart>
 
-    <h3 class="echart-ttl">饼状图数据分析</h3>
+    <!-- <h3 class="echart-ttl">饼状图数据分析</h3>
     <div class="pie-box">
         <div>
             <pie-chart echartId="pie"></pie-chart>
         </div>
-    </div>
+    </div> -->
 
-	 <h3 class="echart-ttl">饼状图数据分析</h3>
+    <h3 class="echart-ttl">饼状图数据分析</h3>
     <div class="pie-box">
         <div>
             <pie-chart-2 echartId="pie-2"></pie-chart-2>
+        </div>
+    </div>
+
+    <h3 class="echart-ttl">线状图数据分析</h3>
+    <line-chart></line-chart>
+
+    <h3 class="echart-ttl">饼状图数据分析</h3>
+    <div class="pie-box">
+        <div>
+            <pie-chart-3 echartId="pie-3"></pie-chart-3>
         </div>
     </div>
 
@@ -26,8 +34,9 @@
 <script>
 import LineChart from "../components/echarts/line.vue";
 import BarChart from "../components/echarts/bar.vue";
-import PieChart from "../components/echarts/pie.vue"
-import PieChart2 from "../components/echarts/pie-2.vue"
+//import PieChart from "../components/echarts/pie.vue";
+import PieChart2 from "../components/echarts/pie-2.vue";
+import PieChart3 from "../components/echarts/pie-3.vue"
 
 export default {
     data() {
@@ -42,8 +51,9 @@ export default {
     components: {
         LineChart,
         BarChart,
-        PieChart,
-        PieChart2
+        // PieChart,
+        PieChart2,
+        PieChart3
     }
 }
 </script>
@@ -53,13 +63,12 @@ export default {
     text-align: center;
     color: #777;
     padding: 5px;
-	font-size: 18px;
+    font-size: 18px;
 }
 
 .pie-box {
     display: flex;
     justify-content: space-around;
-	
 
     >div {
         flex: 1;
